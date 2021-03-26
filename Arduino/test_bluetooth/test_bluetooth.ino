5c4cffcc3cc23587d35105fedc04f451c5218b96
Elliott Pryor <pryorelliott@gmail.com>
2021-03-26 00:57:48 -0600

    Messing around with CountdownTimers

--- Arduino/test_bluetooth/test_bluetooth.ino
+++ Arduino/test_bluetooth/test_bluetooth.ino
@@ -8,8 +8,8 @@ void setup() {
 void loop(){
   if(Serial.available() > 0){  // Send data only when you receive data:
     data = Serial.read();      //Read the incoming data and store it into variable data
-    Serial.print(data);        //Print Value inside data in Serial monitor
-    Serial.print("\n");        //New line 
+    //Serial.print(data);        //Print Value inside data in Serial monitor
+    //Serial.print("\n");        //New line 
     Serial.print("Hello\n");
     if(data == '1')            //Checks whether value of data is equal to 1 
     digitalWrite(LED_BUILTIN, HIGH);  //If value is 1 then LED turns ON
